64265ce9948bac28cb2599bd33d8755662ae8585
matinKH <matinkhamseh5@gmail.com>
2025-03-26 23:15:16 +0330

    add query for get_system_info

--- src/repository/system_info.rs
+++ src/repository/system_info.rs
@@ -1,9 +1,18 @@
-use futures::stream;
-use influxdb2::models::DataPoint;
-use sysinfo::System;
+use influxdb2::models::Query;
 
 use crate::repository::get_connection;
 
-pub async fn get_system_info() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn _get_system_info() -> Result<(), Box<dyn std::error::Error>> {
+    let _connection = get_connection().await?;
+    let qs = format!(
+        "from(bucket: \"stock-prices\")
+        |> range(start: -1w)
+        |> filter(fn: (r) => r.ticker == \"{}\")
+        |> last()
+    ",
+        "AAPL"
+    );
+    let _influx_query = Query::new(qs.to_string());
+
     Ok(())
 }
