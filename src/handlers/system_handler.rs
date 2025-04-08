0d72ad1cc8142f6b09ec08309422539460807570
matinKH <matinkhamseh5@gmail.com>
2025-04-08 12:40:45 +0330

    time range support for get_cpu

--- src/handlers/system_handler.rs
+++ src/handlers/system_handler.rs
@@ -1,24 +1,28 @@
 use std::fmt::Debug;
 
 use crate::{repository::system_info::get_cpu_info, services::system_info::fetch_system_info};
-use axum::{Json, Router, extract::Query, response::IntoResponse, routing::get};
+use axum::{
+    Json, Router,
+    extract::Query,
+    response::{self, IntoResponse, Response},
+    routing::get,
+};
 use chrono::{Date, DateTime, NaiveDate, Utc};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 struct DateRange {
     start: String,
+    stop: String,
 }
 
-pub async fn get_cpu(date_range: Query<DateRange>) -> impl IntoResponse {
+pub async fn get_cpu(date_range: Query<DateRange>) -> Response {
     println!("date_range {date_range:?}");
-    let dt = NaiveDate::parse_from_str(&date_range.start, "%Y-%m-%d");
-    if dt.is_err() {
-        return Json("wrongData");
-    }
-    println!("{dt:?}");
-    let info = get_cpu_info(dt, dt).await.unwrap();
-    return info;
+    let d_start = NaiveDate::parse_from_str(&date_range.start, "%Y-%m-%d");
+    let d_end = NaiveDate::parse_from_str(&date_range.stop, "%Y-%m-%d");
+
+    let info = get_cpu_info(d_start, d_end).await.unwrap();
+    return info.into_response();
 }
 
 pub async fn get_ram() -> impl IntoResponse {
